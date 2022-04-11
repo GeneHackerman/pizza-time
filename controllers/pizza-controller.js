4ff2afc02d4a1288b0c0f335591c2f27d8d6eadb
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-04-11 11:40:05 -0400

    added pizza model and routes

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,69 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+    // the functions will go in here as methods
+    // get all pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // get on pizza by id
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+            .then(dbPizzaData => {
+                // if no pizza is found, send 404
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // create pizza
+    createPizza({ body }, res) {
+        Pizza.create(body)
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    // update pizza by id
+    // findOneAndUpdate will find single document to update
+    // setting new to true will return new ver of doc
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // delete pizza
+    // findOneAndDelete() will retrun doc and delete
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+}
+
+module.exports = pizzaController;
---
+++ models/Pizza.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// create the pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// exports the Pizza model
+module.exports = Pizza;
